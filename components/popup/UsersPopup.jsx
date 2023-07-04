76428593bb645ff9b5491fe165aba6eebca52f07
Akbarjon03 <akbarjonodilov03@gmail.com>
2023-07-04 12:11:39 +0500

    chat

--- components/popup/UsersPopup.jsx
+++ components/popup/UsersPopup.jsx
@@ -80,28 +80,29 @@ const UsersPopup = (props) => {
             <Search />
 
             <div className="mt-5 flex flex-col gap-2 grow relative overflow-auto scrollbar">
-                <div className="absolute w-full">
-                    {users &&
-                        Object.values(users).map((user) => (
-                            <div
-                                onClick={() => handleSelect(user)}
-                                className="flex items-center gap-4 rounded-xl hover:bg-c5 py-2 px-4 cursor-pointer"
-                            >
-                                <Avatar size="large" user={user} />
-                                <div className="flex flex-col gap-1 grow">
-                                    <span className="text-base text-white flex  items-center justify-between">
-                                        <div className="font-medium">
-                                            {user.displayName}
-                                        </div>
-                                    </span>
-                                    <p className="text-sm text-c3">
-                                        {user.email}
-                                    </p>
-                                </div>
+    {users && (
+        <div className="absolute w-full">
+            {Object.values(users).map((user) => (
+                <div
+                    key={user.id} // Add a unique key prop to the parent div
+                    onClick={() => handleSelect(user)}
+                    className="flex items-center gap-4 rounded-xl hover:bg-c5 py-2 px-4 cursor-pointer"
+                >
+                    <Avatar size="large" user={user} />
+                    <div className="flex flex-col gap-1 grow">
+                        <span className="text-base text-white flex items-center justify-between">
+                            <div className="font-medium">
+                                {user.displayName}
                             </div>
-                        ))}
+                        </span>
+                        <p className="text-sm text-c3">{user.email}</p>
+                    </div>
                 </div>
-            </div>
+            ))}
+        </div>
+    )}
+</div>
+
         </PopupWrapper>
     );
 };
